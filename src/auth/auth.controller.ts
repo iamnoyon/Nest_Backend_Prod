4423b100e3ee2d7087ed1625047a4248871720e1
Noyon Sarker <noyon@ba-systems.com>
2026-06-24 16:47:08 +0600

    implemented correct http status

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { SignupDto } from './dto/signUp.dto';
 
@@ -7,6 +7,7 @@ export class AuthController {
   constructor(private readonly userService: UsersService) {}
 
   @Post('/signup')
+  @HttpCode(HttpStatus.CREATED)
   userSignUp(@Body() signupdto: SignupDto) {
     return this.userService.user_sign_up(signupdto);
   }
