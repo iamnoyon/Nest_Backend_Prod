5fc8d1b484035960d18fd5aed0c8cede1fbbd743
Noyon Sarker <noyon@ba-systems.com>
2026-06-24 15:46:38 +0600

    database connection setup done

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,24 +4,32 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthController } from './auth/auth.controller';
+import { UsersController } from './users/users.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'password',
-      database: 'nestdb',
-
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: config.get('DB_PORT'),
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
     UsersModule,
     AuthModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController, AuthController, UsersController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
