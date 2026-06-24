7ec21b97c9c3f566f1b683fa624f273ec6a01e1a
Noyon Sarker <noyon@ba-systems.com>
2026-06-24 14:54:03 +0600

    project setup

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'nestdb',
+
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsersModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Customer Service Management')
+    .setDescription('This is a service provider application')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
